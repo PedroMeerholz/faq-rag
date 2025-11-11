99f55fa9ca300eb3675641e570350c0083919f3e
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2025-11-11 17:25:08 -0300

    refactor: Adaptado o script de criação do RAG para utilização do Pinecone

--- src/rag/create_rag.py
+++ src/rag/create_rag.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.vectorstores import FAISS
+from langchain_pinecone import PineconeVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
@@ -10,6 +10,7 @@ load_dotenv()
 docs_path = os.environ['DOCUMENTS_PATH']
 rag_path = os.environ['RAG_PATH']
 embedding_model = os.environ['EMBEDDING_MODEL']
+index_name = os.environ['PINECONE_INDEX_NAME']
 
 embeddings = HuggingFaceEmbeddings(model=embedding_model)
 
@@ -17,8 +18,14 @@ loader = DirectoryLoader(docs_path, glob="**/*.pdf", loader_cls=PyPDFLoader)
 documents = loader.load()
 
 chunck_size = 1000
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=chunck_size*0.15)
+text_splitter = CharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=chunck_size*0.15
+)
 documents = text_splitter.split_documents(documents)
 
-vectorstore = FAISS.from_documents(documents, embeddings)
-vectorstore.save_local(rag_path)
+vectorstore = PineconeVectorStore.from_documents(
+    documents=documents,
+    embedding=embeddings,
+    index_name=index_name
+)
